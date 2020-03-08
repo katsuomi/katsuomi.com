7364abb38f5b84630f9bc46463a2b8302391e41c
katsuomi <mugichak1227@gmail.com>
2020-03-08 22:33:03 +0900

    update articleMethods.ts

---
+++ src/methods/articleMethods.ts
@@ -0,0 +1,22 @@
+/**
+ *
+ * articleMethods
+ *
+ */
+
+import * as firebase from "firebase";
+import "firebase/firestore";
+// import models
+import * as Model from "models/articleModel";
+
+// 記事の追加
+export const createArticle = (data: any) => {
+  // firebase
+  //   .firestore()
+  //   .collection("tags")
+  //   .doc(text)
+  //   .set({
+  //     text: text,
+  //     id: text
+  //   });
+};
