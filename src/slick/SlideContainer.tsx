c77ae838378bc27b9a6ace9808026ea0c3cdcc95
katsuomi <mugichak1227@gmail.com>
2020-03-22 18:45:01 +0900

    update slider

---
+++ src/slick/SlideContainer.tsx
@@ -0,0 +1,120 @@
+import React, { FC, useEffect, FormEvent } from "react";
+import Slider from "react-slick";
+import styled from "styled-components";
+import { connect } from "react-redux";
+import _ from "lodash";
+import { bindActionCreators, Dispatch } from "redux";
+
+// import css
+import "./slick.css";
+
+// import atoms
+import Anchor from "components/atoms/Anchor";
+
+// import commons
+import Spinner from "components/commons/Spinner";
+
+// import actions
+import { getSlideShowArticles } from "actions/articleAction";
+
+// import utils
+import * as breakPoints from "utils/breakPoints";
+
+// import models
+import { AppState } from "models/index";
+import * as adminLoginModel from "models/adminLoginModel";
+import * as articleModel from "models/articleModel";
+
+interface StateProps {
+  slideShowArticles?: articleModel.Article[];
+  isLoading?: boolean;
+}
+
+interface DispatchProps {
+  getSlideShowArticles: () => void;
+}
+
+type DefaultProps = StateProps & DispatchProps;
+
+const Wrapper = styled.div``;
+
+const Img = styled.img`
+  width: 98%;
+  height: ${breakPoints.hasWidth800() ? "30vw" : "40vw"};
+  max-height: 390px;
+`;
+
+const SlideContainer: FC<DefaultProps> = ({
+  slideShowArticles,
+  isLoading,
+  getSlideShowArticles
+}) => {
+  useEffect(() => {
+    getSlideShowArticles();
+  }, [getSlideShowArticles]);
+  console.log("slideShowArticles:", slideShowArticles);
+  console.log("isLoading:", isLoading);
+  const settings = {
+    centerMode: true,
+    centerPadding: "300px",
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: breakPoints.tabletMaxWidth,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: "200px"
+        }
+      },
+      {
+        breakpoint: breakPoints.tabletMinWidth,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: "100px"
+        }
+      }
+    ]
+  };
+
+  return (
+    <>
+      {isLoading ? (
+        <Spinner top={breakPoints.isSmartPhone() ? "30%" : "25%"} />
+      ) : (
+        <Slider {...settings}>
+          {slideShowArticles &&
+            slideShowArticles.map((article, i) => (
+              <Wrapper key={i}>
+                <Anchor src={"/articles/" + article.uid}>
+                  <Img src={article.thumbnail_image_path} alt={article.title} />
+                </Anchor>
+              </Wrapper>
+            ))}
+        </Slider>
+      )}
+    </>
+  );
+};
+
+const mapStateToProps = (state: AppState) => ({
+  slideShowArticles: state.article.slideShowArticles,
+  isLoading: state.article.isLoading
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) =>
+  bindActionCreators(
+    {
+      getSlideShowArticles: () => getSlideShowArticles.start()
+    },
+    dispatch
+  );
+
+export default connect(mapStateToProps, mapDispatchToProps)(SlideContainer);
