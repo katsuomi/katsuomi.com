6603263947134324727efa99fa0c4624e1e42854
katsuomi <mugichak1227@gmail.com>
2020-03-22 16:50:21 +0900

    update method

--- src/methods/articleMethods.ts
+++ src/methods/articleMethods.ts
@@ -8,15 +8,3 @@ import * as firebase from "firebase";
 import "firebase/firestore";
 // import models
 import * as Model from "models/articleModel";
-
-// 記事の追加
-export const createArticle = (data: any) => {
-  // firebase
-  //   .firestore()
-  //   .collection("tags")
-  //   .doc(text)
-  //   .set({
-  //     text: text,
-  //     id: text
-  //   });
-};
