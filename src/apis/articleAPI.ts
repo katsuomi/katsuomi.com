c51f9206a8a60f7b03f0ecd514b1bd9dcf160658
katsuomi <mugichak1227@gmail.com>
2020-03-22 18:43:39 +0900

    update API

--- src/apis/articleAPI.ts
+++ src/apis/articleAPI.ts
@@ -19,3 +19,35 @@ export const createAtricle = async (payload: Model.Article) => {
     return { error };
   }
 };
+
+export const getSlideShowArticles = async () => {
+  try {
+    const articles: Model.Article[] = [];
+    await firebase
+      .firestore()
+      .collection("articles")
+      .where("is_add_slide_show", "==", true)
+      .get()
+      .then(snapshot => {
+        if (snapshot.empty) {
+          return;
+        }
+        snapshot.forEach(doc => {
+          articles.push({
+            uid: doc.id,
+            content: doc.data().content,
+            title: doc.data().title,
+            date: doc.data().date,
+            tag_ids: doc.data().tag_ids,
+            thumbnail_image_path: doc.data().thumbnail_image_path
+          });
+        });
+      })
+      .catch(err => {
+        throw new Error(err.message);
+      });
+    return { articles };
+  } catch (error) {
+    return { error };
+  }
+};
