9d061a80a751e9142087c291e5298521de795285
katsuomi <mugichak1227@gmail.com>
2020-03-23 00:02:14 +0900

    update home-component

---
+++ src/components/organisms/HomeCenterSide.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 60%;
+`;
+
+const HomeCenterSide: FC = () => (
+  <>
+    <Wrapper>HomeCenterSide</Wrapper>
+  </>
+);
+
+export default HomeCenterSide;
---
+++ src/components/organisms/HomeRightSide.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 20%;
+`;
+
+const HomeRightSide: FC = () => (
+  <>
+    <Wrapper>HomeRightSide</Wrapper>
+  </>
+);
+
+export default HomeRightSide;
