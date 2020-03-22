f10ba2efcfbce8999744a51018a6552bd1f6e38f
katsuomi <mugichak1227@gmail.com>
2020-03-23 00:01:58 +0900

    update spinner

--- src/components/commons/Spinner.tsx
+++ src/components/commons/Spinner.tsx
@@ -9,20 +9,25 @@ import Loader from "react-loader-spinner";
 import { Dimmer, Segment } from "semantic-ui-react";
 import styled from "styled-components";
 
+//import utils
+import * as colors from "utils/color";
+
 export interface SpinnerProps {
   top?: string;
+  left?: string;
   position?: "abslute" | "relative";
 }
 
 export interface SpinnerStyleProps {
   top?: string;
+  left?: string;
   position?: "abslute" | "relative";
 }
 
 const SegmentWrapper = styled.div<SpinnerStyleProps>`
   position: ${props => (props.position ? props.position : "absolute")};
   top: ${props => (props.top ? props.top : "50%")};
-  left: 50%;
+  left: ${props => (props.left ? props.left : "50%")};
   margin-right: -50%;
   transform: translate(-50%, -50%);
   > * > * {
@@ -30,10 +35,16 @@ const SegmentWrapper = styled.div<SpinnerStyleProps>`
   }
 `;
 
-const LoadWrapper = styled.p``;
+const LoadWrapper = styled.p`
+  color: ${colors.BLACK};
+`;
 
-const Spinner: FC<SpinnerProps> = ({ top, position }) => (
-  <SegmentWrapper top={top ? top : ""} position={position}>
+const Spinner: FC<SpinnerProps> = ({ top, left, position }) => (
+  <SegmentWrapper
+    top={top ? top : ""}
+    left={left ? left : ""}
+    position={position}
+  >
     <Segment className="spinner">
       <Dimmer active inverted>
         <LoadWrapper>読み込み中...</LoadWrapper>
