a8f9331b540d62f47a2f2fb9c3c142e6988ea37b
katsuomi <mugichak1227@gmail.com>
2020-03-22 16:50:41 +0900

    add model

--- src/models/articleModel.ts
+++ src/models/articleModel.ts
@@ -8,11 +8,34 @@
 import * as ActionTypes from "constants/actionTypes";
 
 export interface Article {
-  uid: string;
+  uid?: string;
   content: string;
   title: string;
-  day: Date;
+  date: Date;
   tag_ids: string[];
   thumbnail_image_path: string;
-  favorite_count: number;
+  favorite_count?: number;
+  is_add_slide_show: boolean;
 }
+
+export interface ArticleState {
+  isLoading: boolean;
+}
+
+export interface CreateArticleStartAction {
+  type: typeof ActionTypes.CREATE_ARTICLE_START;
+  payload: Article;
+}
+
+export interface CreateArticleSuccessAction {
+  type: typeof ActionTypes.CREATE_ARTICLE_SUCCESS;
+}
+
+export interface CreateArticleFailureAction {
+  type: typeof ActionTypes.CREATE_ARTICLE_FAILURE;
+}
+
+export type ArticleAction =
+  | CreateArticleStartAction
+  | CreateArticleSuccessAction
+  | CreateArticleFailureAction;
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,8 +1,12 @@
 // import models
 import { FlashMessageState } from "models/flashMessagesModel";
 import { AdminLoginState } from "models/adminLoginModel";
+import { ContactFormState } from "models/contactFormModel";
+import { ArticleState } from "models/articleModel";
 
 export interface AppState {
   flashMessages: FlashMessageState;
   adminLogin: AdminLoginState;
+  contactForm: ContactFormState;
+  article: ArticleState;
 }
