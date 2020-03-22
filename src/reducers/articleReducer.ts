6d40f7937184db11e29dce8f46e6f4463177a8f7
katsuomi <mugichak1227@gmail.com>
2020-03-22 18:45:53 +0900

    update reducer

--- src/reducers/articleReducer.ts
+++ src/reducers/articleReducer.ts
@@ -7,7 +7,8 @@ import * as ActionTypes from "constants/actionTypes";
 import * as Model from "models/articleModel";
 
 const initialState: Model.ArticleState = {
-  isLoading: false
+  isLoading: false,
+  slideShowArticles: []
 };
 
 const article: Reducer<Model.ArticleState, Model.ArticleAction> = (
@@ -26,6 +27,22 @@ const article: Reducer<Model.ArticleState, Model.ArticleAction> = (
         isLoading: false
       };
     case ActionTypes.CREATE_ARTICLE_FAILURE:
+      return {
+        ...state,
+        isLoading: false
+      };
+    case ActionTypes.GET_SLIDE_SHOW_ARTICLES_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case ActionTypes.GET_SLIDE_SHOW_ARTICLES_SUCCESS:
+      return {
+        ...state,
+        slideShowArticles: action.payload,
+        isLoading: false
+      };
+    case ActionTypes.GET_SLIDE_SHOW_ARTICLES_FAILURE:
       return {
         ...state,
         isLoading: false
