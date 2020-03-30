944e31efa8cb34d6b4c6294208709e93f5eff37c
katsuomi <mugichak1227@gmail.com>
2020-03-30 19:58:19 +0900

    create method date to string

--- src/methods/articleMethods.ts
+++ src/methods/articleMethods.ts
@@ -4,7 +4,22 @@
  *
  */
 
-import * as firebase from "firebase";
 import "firebase/firestore";
 // import models
 import * as Model from "models/articleModel";
+
+class TimeStamp {
+  seconds!: number;
+  nanoseconds!: number;
+}
+
+// 日付を文字列に
+export const dateToString = (date: Date | TimeStamp): string => {
+  if (date instanceof Date) {
+    return "undefined";
+  }
+  const dateTime = new Date(date.seconds * 1000);
+  const result = dateTime.toLocaleDateString();
+
+  return result;
+};
