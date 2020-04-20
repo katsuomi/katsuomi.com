c1e0cc1b92f552c143936f3ed380b11f152e0687
青木克臣 <mugichak1227@gmail.com>
2020-04-20 20:18:58 +0900

    add react-ga

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from "react";
-import { Redirect, Route, Switch } from "react-router";
+import React, { FC, useEffect } from "react";
+import { Redirect, Route, Switch, withRouter, RouteComponentProps } from "react-router";
 import styled from "styled-components";
+import ReactGA from 'react-ga';
 
 // import components
 import Header from "components/organisms/header/Header";
@@ -15,32 +16,39 @@ const Wrapper = styled.div`
   margin-right: ${breakPoints.isIPhone5s() ? "-19px" : "0"};
 `;
 
-const App: FC = () => (
-  <Wrapper>
-    <Header />
-    <Switch>
-      <Route path={pages.home.path} component={routes.Home} exact />
-      <Route path={pages.admin.path} component={routes.Admin} exact />
-      <Route
-        path={pages.adminCreateArticle.path}
-        component={routes.AdminCreateArticle}
-        exact
-      />
-      <Route
-        path={pages.adminArticles.path}
-        component={routes.AdminArticles}
-        exact
-      />
-      <Route path={pages.article.path} component={routes.Article} exact />
-      <Route path={pages.articleEdit.path} component={routes.ArticleEdit} exact />
-      <Route path={pages.tag.path} component={routes.Tag} exact />
-      <Route path={pages.contact.path} component={routes.Contact} exact />
-      <Route path={pages.profile.path} component={routes.Profile} exact />
-      <Route path={pages.articleOgp.path} component={routes.Article} exact />
+const App: FC<RouteComponentProps> = ({ location }) => {
+  useEffect(() => {
+    const { pathname } = location;
+    ReactGA.set({ page: pathname });
+    ReactGA.pageview(pathname);
+  }, []);
+  return (
+    <Wrapper>
+      <Header />
+      <Switch>
+        <Route path={pages.home.path} component={routes.Home} exact />
+        <Route path={pages.admin.path} component={routes.Admin} exact />
+        <Route
+          path={pages.adminCreateArticle.path}
+          component={routes.AdminCreateArticle}
+          exact
+        />
+        <Route
+          path={pages.adminArticles.path}
+          component={routes.AdminArticles}
+          exact
+        />
+        <Route path={pages.article.path} component={routes.Article} exact />
+        <Route path={pages.articleEdit.path} component={routes.ArticleEdit} exact />
+        <Route path={pages.tag.path} component={routes.Tag} exact />
+        <Route path={pages.contact.path} component={routes.Contact} exact />
+        <Route path={pages.profile.path} component={routes.Profile} exact />
+        <Route path={pages.articleOgp.path} component={routes.Article} exact />
 
-      <Redirect to="/" />
-    </Switch>
-  </Wrapper>
-);
+        <Redirect to="/" />
+      </Switch>
+    </Wrapper>
+  );
+};
 
-export default App;
+export default withRouter<any, any>(App);
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,19 @@ import { hydrate, render } from "react-dom";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from 'connected-react-router';
 import createBrowserHistory from 'history/createBrowserHistory';
+import ReactGA from 'react-ga';
 
 import App from "App";
 import * as serviceWorker from "serviceWorker";
 import configureStore from './store';
 
+ReactGA.initialize('UA-163444940-1');
 export const history = createBrowserHistory();
+history.listen(({ pathname }) => {
+  ReactGA.set({ page: pathname });
+  ReactGA.pageview(pathname);
+});
+
 export const store = configureStore(history);
 
 const rootElement: HTMLElement | null = document.getElementById("root");
